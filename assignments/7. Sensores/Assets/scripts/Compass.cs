8645fe4340c0fecd6f9bf52fe9e19d02e9c72cb2
AlberTJ97 <alu0100949568@ull.edu.es>
2018-12-13 11:29:26 +0000

    Bug: fixed GPS bug

--- assignments/7. Sensores/Assets/scripts/Compass.cs
+++ assignments/7. Sensores/Assets/scripts/Compass.cs
@@ -14,34 +14,10 @@ public class Compass : MonoBehaviour {
 	void Start () {
         Instance = this;
         DontDestroyOnLoad(gameObject);
-        StartCoroutine(StartLocationService());
 	}
 	
-    private IEnumerator StartLocationService() {
-        if (Input.location.isEnabledByUser) {
-            Debug.Log("User has not enabled GPS.");
-            yield break;
-        }
-        Input.location.Start(10, 0.01f);
-        Input.compass.enabled = true;
-
-        int maxWait = 20;
-        while (Input.location.status == LocationServiceStatus.Initializing 
-               && maxWait > 0) {
-            yield return new WaitForSeconds(1);
-            maxWait -= 1;
-        }
-        if (maxWait < 1) {
-            Debug.Log("Timed out.");
-            yield break;
-        }
-        if (Input.location.status == LocationServiceStatus.Failed)
-        {
-            Debug.Log("Unable to determine device location.");
-            yield break;
-        }
-        Debug.Log(Input.location.status);
-        Debug.Log("location service loadeda");
-        trueHeading = Input.compass.trueHeading;
+    public void UpdateCompass()
+    {
+        this.trueHeading = Input.compass.trueHeading;
     }
 }
--- assignments/7. Sensores/Assets/scripts/GPS.cs
+++ assignments/7. Sensores/Assets/scripts/GPS.cs
@@ -18,45 +18,13 @@ public class GPS : MonoBehaviour {
     void Start() {
         Instance = this;
         DontDestroyOnLoad(gameObject);
-        StartCoroutine(StartLocationService());
     }
 
-    private IEnumerator StartLocationService() {
-        if (isUnityRemote)
-        {
-            yield return new WaitForSeconds(5);
-        }
-
-        if (Input.location.isEnabledByUser) {
-            Debug.Log("User has not enabled GPS.");
-            yield break;
-        }
-
-        Input.location.Start(1, 0.01f);
-        if (isUnityRemote)
-        {
-            yield return new WaitForSeconds(5);
-        }
-        int maxWait = 20;
-        while(Input.location.status != LocationServiceStatus.Running && maxWait > 0) {
-            yield return new WaitForSeconds(1);
-            maxWait -= 1;
-        }
-
-        if (maxWait <= 0) {
-            Debug.Log("Timed out.");
-            yield break;
-        }
-
-        if (Input.location.status == LocationServiceStatus.Failed) {
-            Debug.Log("Unable to determine device location.");
-            yield break;
-        } else {
-            Debug.Log(Input.location.status);
-            this.latitude = Input.location.lastData.latitude;
-            this.longitude = Input.location.lastData.longitude;
-            this.altitude = Input.location.lastData.altitude;
-            yield break;
-        }
+    public void UpdateGPS()
+    {
+        this.latitude = Input.location.lastData.latitude;
+        this.longitude = Input.location.lastData.longitude;
+        this.altitude = Input.location.lastData.altitude;
     }
 }
+

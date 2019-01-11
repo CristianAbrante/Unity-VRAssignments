2a47c29fe218c75d582ff17b9e3e977ca85d0f69
AlberTJ97 <alu0100949568@ull.edu.es>
2019-01-11 09:08:56 +0000

    Add: added voice recognition assignment

---
+++ assignments/9. Reconocimiento de voz/Assets/Scripts/DictationScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class DictationScript : MonoBehaviour
+{
+    [SerializeField]
+    private Text m_Recognitions;
+
+    private DictationRecognizer m_DictationRecognizer;
+    private bool isListening = false;
+    public Button startDictationButton;
+
+    void Start()
+    {
+        m_DictationRecognizer = new DictationRecognizer();
+
+        m_DictationRecognizer.DictationResult += (text, confidence) =>
+        {
+            print(text);
+
+            if (m_Recognitions != null)
+                m_Recognitions.text += text + "\n";
+        };
+
+        m_DictationRecognizer.DictationComplete += (completionCause) =>
+        {
+            if (completionCause != DictationCompletionCause.Complete)
+                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
+        };
+    }
+
+    public void startDictation()
+    {
+        if (!isListening)
+        {
+            isListening = !isListening;
+            GetComponent<Image>().color = new Color(0.9f, 0.4f, 0.3f);
+            GetComponentInChildren<Text>().text = "Listening...";
+            m_DictationRecognizer.Start();
+        }
+        else
+        {
+            isListening = !isListening;
+            m_DictationRecognizer.Stop();
+            m_DictationRecognizer.Dispose();
+            GetComponentInChildren<Text>().text = "Finish";
+            startDictationButton.interactable = !startDictationButton.interactable;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        m_DictationRecognizer.Dispose();
+    }
+}
---
+++ assignments/9. Reconocimiento de voz/Assets/Scripts/KeywordScript.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class KeywordScript : MonoBehaviour
+{
+    [SerializeField]
+
+    private KeywordRecognizer m_Recognizer;
+    private string[] m_Keywords = { "hola" };
+    private bool isListening = false;
+    public Button startListeningKeyword;
+
+    void Start()
+    {
+        m_Recognizer = new KeywordRecognizer(m_Keywords );
+        m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+    }
+
+    public void startListening()
+    {
+        if (!isListening)
+        {
+            isListening = !isListening;
+            m_Recognizer.Start();
+            GetComponent<Image>().color = new Color(0.9f, 0.4f, 0.3f);
+            GetComponentInChildren<Text>().text = "Listening...";
+        }
+        else
+        {
+            isListening = !isListening;
+            m_Recognizer.Stop();
+            m_Recognizer.Dispose();
+            GetComponentInChildren<Text>().text = "Finish";
+            startListeningKeyword.interactable = !startListeningKeyword.interactable;
+        }
+    }
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.AppendFormat("{0} ({1}){2}", args.text, args.confidence, Environment.NewLine);
+        builder.AppendFormat("\tTimestamp: {0}{1}", args.phraseStartTime, Environment.NewLine);
+        builder.AppendFormat("\tDuration: {0} seconds{1}", args.phraseDuration.TotalSeconds, Environment.NewLine);
+        Debug.Log(builder.ToString());
+    }
+}
